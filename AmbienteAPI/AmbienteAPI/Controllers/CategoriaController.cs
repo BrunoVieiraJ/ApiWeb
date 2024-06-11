58fa33f8ad3b5ba2269caa7e3fdef8219d078666
BrunoVieiraj <brunov.julio@outlook.com>
2024-06-10 21:39:48 -0300

    trabalho

---
+++ AmbienteAPI/AmbienteAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,114 @@
+﻿using AmbienteAPI.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebAPI.Model;
+using WebAPI.Controllers;
+
+namespace AmbienteAPI.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public CategoriaController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categoria = await _apiService.GetAllCategoriasAsync();
+            return View(categoria);
+        }
+
+
+        //create
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Categoria categoria)
+        {
+            _apiService.PostCategoriaAsync(categoria);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> Details(long? id)
+        {
+            var categoria = await _apiService.GetCategoriaAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        //CORRIGIR O EDIT DA CATEGORIA
+        public async Task<IActionResult> Edit(long? id)
+        {
+            var categoria = await _apiService.GetCategoriaAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long? id, Categoria categoria)
+        {
+            if (id != categoria.CategoriasId)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var response = await _apiService.PutCategoriaAsync(id, categoria);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                ModelState.AddModelError(string.Empty, "Ocorreu um erro ao atualizar o produto.");
+            }
+
+            return View(categoria);
+        }
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            var categoria = await _apiService.GetCategoriaAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+
+            var response = await _apiService.DeleteCategoria(id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+        }
+
+
+
+        
+
+
+    }
+}
---
+++ AmbienteAPI/AmbienteAPI/Service/ApiService.cs
@@ -0,0 +1,86 @@
+﻿using WebAPI.Model;
+
+namespace AmbienteAPI.Service
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Produto>> GetAllProdutosAsync()
+        {
+            var response = await _httpClient.GetAsync("/api/Produtos");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<List<Produto>>();
+        }
+
+        public async Task<Produto> GetProdutoAsync(int id)
+        {
+            var response = await _httpClient.GetAsync($"/api/Produtos/{id}");    
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<Produto>();
+        }
+
+        public async Task<HttpResponseMessage> PostProdutoAsync(Produto produto)
+        {
+            return await _httpClient.PostAsJsonAsync("api/Produtos", produto);
+        }
+
+        public async Task<HttpResponseMessage> PutProdutoAsync(int id, Produto produto)
+        {
+            return await _httpClient.PutAsJsonAsync($"/api/Produtos/{id}", produto);
+        }
+
+        //public async Task<HttpResponseMessage> DeleteProdutoAsync(int id)
+        //{
+        //    return await _httpClient.DeleteAsync($"/api/Produtos/{id}");
+        //}
+        public async Task<HttpResponseMessage> DeleteProdutoAsync(int id)
+        {
+            return await _httpClient.DeleteAsync($"api/Produtos?id={id}");
+        }
+
+        //--------------------------------------------------------------------------------- pra eu n me perder
+
+        public async Task<List<Categoria>> GetAllCategoriasAsync()
+        {
+            var response = await _httpClient.GetAsync("/api/Categoria");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<List<Categoria>>();
+        }
+
+        public async Task<Categoria> GetCategoriaAsync(long? id)
+        {
+            var response = await _httpClient.GetAsync($"/api/Categoria/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<Categoria>();
+        }
+
+        public async Task<HttpResponseMessage> PostCategoriaAsync(Categoria categoria)
+        {
+            return await _httpClient.PostAsJsonAsync("/api/Categoria", categoria);
+        }
+
+        public async Task<HttpResponseMessage> PutCategoriaAsync(long? id, Categoria categoria)
+        {
+            return await _httpClient.PutAsJsonAsync($"/api/Categoria/{id}", categoria);
+        }
+
+        public async Task<HttpResponseMessage> DeleteCategoria(long? id)
+        {
+            return await _httpClient.DeleteAsync($"api/Produtos?id={id}");
+
+        }
+
+
+
+
+
+
+
+    }
+}
